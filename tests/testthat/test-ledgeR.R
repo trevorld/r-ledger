63955848081367206a4a91ce5bc69811e7549945
Trevor Davis <trevor.l.davis@gmail.com>
2018-02-14 17:03:25 -0800

    Add test skip; tweak .travis.yml

--- tests/testthat/test-ledgeR.R
+++ tests/testthat/test-ledgeR.R
@@ -2,15 +2,18 @@
 
 context("Testing reading ledger register works")
 test_that("register works as expected", {
+    skip_if_not(any(Sys.which("ledger") != ""), "ledger not on path")
     register(system.file("extdata", "example.ledger", package = "ledgeR"))
 })
 
 context("Testing reading hledger register works")
 test_that("register works as expected", {
+    skip_if_not(any(Sys.which("hledger") != ""), "hledger not on path")
     register(system.file("extdata", "example.hledger", package = "ledgeR"))
 })
 
 context("Testing reading beancount register works")
 test_that("register works as expected", {
+    skip_if_not(any(Sys.which("bean-report") != ""), "bean-report not on path")
     register(system.file("extdata", "example.beancount", package = "ledgeR"))
 })
